ef7a16f60b047b679e7f5235a2680fdd17146a04
Arnel Labarda <aqlx86@gmail.com>
2017-06-03 19:43:11 +0800

    big bang

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Arnel Q. Labarda</title>
+    <meta name="description" content="Web Aplication Developer, Freelancer. Particularly interested in Open Source Applications" />
+
+    <link href="./css/style.css" rel="stylesheet" type="text/css" charset="utf-8" />
+</head>
+
+<body>
+
+<div id='wrapper'>
+    <div id='content'>
+        <div id='image'>
+            <img src="/images/me.jpg" alt="me" title="arnel labarda" />
+        </div>
+        <div id='info'>
+            <h2>Arnel Labarda</h2>
+
+            <blockquote>"I swim againts the current because only dead fish go with the flow."</blockquote>
+
+            <ul>
+                <li><a href="http://arnel.labarda.ph" target="blank">photos</a> &middot;</li>
+                <li><a href="https://github.com/aqlx86" target="blank">github</a> &middot;</li>
+                <li><a href="http://ph.linkedin.com/in/aqlx86" target="blank">linkedin</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-67526894-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+</body>
+
+</html>
